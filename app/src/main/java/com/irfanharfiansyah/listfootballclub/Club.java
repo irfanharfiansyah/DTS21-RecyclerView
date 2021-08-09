d877e7c4a4d6a8e90f17cae7ae6e65654ac3b157
irfanharfiansyah <Irfanharfiansyah37@gmail.com>
2021-08-09 20:55:55 +0700

    Add model club

---
+++ app/src/main/java/com/irfanharfiansyah/listfootballclub/Club.java
@@ -0,0 +1,38 @@
+package com.irfanharfiansyah.listfootballclub;
+
+public class Club {
+    private String name, description, country;
+    int photo;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public int getPhoto() {
+        return photo;
+    }
+
+    public void setPhoto(int photo) {
+        this.photo = photo;
+    }
+}
